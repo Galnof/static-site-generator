1f56324e65ea9be3a69e2fc2bd1b310e9bf8a30b
Galnof <keedawg25@gmail.com>
2025-04-07 14:05:17 -0500

    Setup initial main files and .gitignore file.

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+def main():
+    TextNode_1 = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(TextNode_1)
+
+if __name__ == "__main__":
+    main()
