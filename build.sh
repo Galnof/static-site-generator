c14802974ddbe8a1131030b6f6b69241216d3aba
Galnof <keedawg25@gmail.com>
2025-05-05 19:33:21 -0500

    Added comments to build.sh and main.sh files.

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
+# Shell script that hosts the site on a GitHub Pages server.
 python3 src/main.py "/static-site-generator/"
--- main.sh
+++ main.sh
@@ -1,2 +1,3 @@
+# Shell script that hosts the site on a local server.
 python3 src/main.py
 cd docs && python3 -m http.server 8888
